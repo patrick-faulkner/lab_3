6208d1072719cfaf843c667aef77f320fa25d0ff
patrick-faulkner <pafa8037@colorado.edu>
2019-09-19 09:47:28 -0600

    Creating practice.sh file and storing regex_practice.sh

---
+++ practice.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Author: Patrick Faulkner
+#Date 9/19/192
+echo "Enter a number: "
+read numOne
+echo "Enter a second number: "
+read numTwo
+sum=$(($numOne + $numTwo))
+echo "The sum is: $sum"
+let prod=numOne*numTwo
+echo "The product is: $prod"
+echo "File Name: $0"
+echo "Command Line Argument 1: $1"
+grep $1 $2
